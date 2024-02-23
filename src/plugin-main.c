5a8dc48d38fb0e53666755535d0868a2792e5c73
Mark the Miner <marktheminer@github.com>
2024-02-23 16:26:32 -0500

    copyright and platform config

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -1,6 +1,6 @@
 /*
 Plugin Name
-Copyright (C) <Year> <Developer> <Email Address>
+Copyright (C) 2024 Mark Jones <mark.jones1112+nospam@gmail.com>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
